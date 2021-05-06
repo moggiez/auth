1fdf370530dbb29dd1cba264b9b847ac55b27809
GeorgiStavrev <georgi@insurify.com>
2021-05-06 09:46:52 +0200

    formats terraform code

--- cognito.tf
+++ cognito.tf
@@ -18,9 +18,9 @@ resource "aws_cognito_user_pool" "_" {
 
   // This is only used if the customMessage trigger is disabled
   verification_message_template {
-    default_email_option  = "CONFIRM_WITH_CODE"
-    email_message = "Please confirm with your code {####}."
-    email_subject = "Your ${var.domain_name} account verification link"
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_message        = "Please confirm with your code {####}."
+    email_subject        = "Your ${var.domain_name} account verification link"
   }
 
   lambda_config {
