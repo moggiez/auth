ee70c10c726416d57899479be5cb764f7ba857de
GeorgiStavrev <georgi@insurify.com>
2021-05-06 11:37:38 +0200

    updated the confirmation url

--- code/cognito_triggers/custom_message/index.js
+++ code/cognito_triggers/custom_message/index.js
@@ -1,6 +1,6 @@
 exports.handler = (event, context, callback) => {
   if (event.triggerSource === "CustomMessage_SignUp") {
-    const confirmUrl = `https://www.moggies.io/auth/signin?action=confirmUser&clientId=${event.callerContext.clientId}user_name=${event.request.userAttributes.sub}&confirmation_code=${event.request.codeParameter}`;
+    const confirmUrl = `https://www.moggies.io/auth/confirm?clientId=${event.callerContext.clientId}&user_name=${event.request.userAttributes.sub}&confirmation_code=${event.request.codeParameter}`;
     const message = `Please click the link below to verify your email address. <a href=${confirmUrl}>Verify Email</a>`;
     event.response.smsMessage =
       "Welcome to the service. Your confirmation code is " +
